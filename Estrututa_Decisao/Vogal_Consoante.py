f36b33ddd2a6d3d3c7e1d841884bd50b6448ffda
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-04 23:54:19 -0300

    Pronto 4

---
+++ Estrututa_Decisao/Vogal_Consoante.py
@@ -0,0 +1,16 @@
+letra=input('Informe uma letra: ')
+if letra.isalpha():
+    if letra=="a":
+        print('Essa letra é uma vogal.')
+    elif letra=="e":
+        print('Essa letra é uma vogal.')
+    elif letra=="i":
+        print('Essa letra é uma vogal.')
+    elif letra=="o":
+        print('Essa letra é uma vogal.')
+    elif letra=="u":
+        print('Essa letra é uma vogal.')
+    else:
+        print('Essa letra é uma consoante.')
+else:
+    print('Não é uma letra.')
