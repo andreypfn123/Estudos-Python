fe28601d710148eef4b904a3e5af7f15e1ee033b
Andrey <andreypfn123@hotmail.com>
2022-09-05 13:55:24 -0300

    Pronto 7

---
+++ Maior_Menor_Numero.py
@@ -0,0 +1,15 @@
+num1=int(input('Digite o primeiro número: '))
+num2=int(input('Digite o segundo número: '))
+num3=int(input('Digite o terceiro número: '))
+maior=num1
+if num2>num1 and num2>num3:
+    maior=num2
+if num3>num1 and num3>num2:
+    maior=num3
+print('O maior número é',maior)
+menor=num1
+if num2<num1 and num2<num3:
+    maior=num2
+if num3<num1 and num3<num2:
+    maior=num3
+print('O menor número é',menor)
