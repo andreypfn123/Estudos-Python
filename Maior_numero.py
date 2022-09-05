a1d74c31ed82a48f7e5b863a8678de84f3f50289
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-04 21:26:40 -0300

    Pronto 1

---
+++ Maior_numero.py
@@ -0,0 +1,8 @@
+numero=int(input('Diga o primeiro número: '))
+numero2=int(input('Diga o segundo número: '))
+if numero>numero2:
+    print('O maior é:',numero)
+elif numero2>numero:
+    print('O maior é:',numero2)
+else:
+    print('São iguais')
