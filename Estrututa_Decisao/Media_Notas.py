1ba5ccc091a511c330c55f8114b0145825231439
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-05 00:12:16 -0300

    Pronto 5

---
+++ Estrututa_Decisao/Media_Notas.py
@@ -0,0 +1,9 @@
+nota=int(input('Digite aqui sua primeira nota: '))
+nota2=int(input('Digite aqui sua segunda nota: '))
+media=(nota+nota2)/2
+if media<7:
+    print('Reprovado')
+elif media>=7 and nota <10:
+    print('Aprovado')
+elif media==10:
+    print('Aprovado com Distinção')
