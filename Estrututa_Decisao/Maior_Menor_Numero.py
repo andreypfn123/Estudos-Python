bed8d6d1203ff0d52d0a0919b9c98f6f4cd06482
Andrey <andreypfn123@hotmail.com>
2022-09-05 14:05:20 -0300

    Pronto 6

--- Estrututa_Decisao/Maior_Menor_Numero.py
+++ Estrututa_Decisao/Maior_Menor_Numero.py
@@ -6,10 +6,10 @@ if num2>num1 and num2>num3:
     maior=num2
 if num3>num1 and num3>num2:
     maior=num3
-print('O maior número é',maior)
+print('O maior número é:',maior)
 menor=num1
 if num2<num1 and num2<num3:
     maior=num2
 if num3<num1 and num3<num2:
     maior=num3
-print('O menor número é',menor)
+print('O menor número é:',menor)
