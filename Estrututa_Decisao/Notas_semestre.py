2c9f975a18ded462f5129e5627a70a3be1b21be6
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-11 00:15:10 -0300

    Fazendo 14

---
+++ Estrututa_Decisao/Notas_semestre.py
@@ -0,0 +1,13 @@
+nota1=float(input('Primeira nota: '))
+nota2=float(input('Segunda nota: '))
+media=(nota1+nota2)/2
+if media >=9.0 and media <= 10:
+    print ('')
+elif media >=7.5 and media <= 9.0:
+    print('')
+elif media >= 6.0 and media <=7.5:
+    print('')
+elif media >= 4.0 and media <= 6.0:
+    print('')
+elif media >= 4.0 and media <= 0:
+    print('')
