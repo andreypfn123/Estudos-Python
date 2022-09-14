e9b304156fa79be66b57bb5c35b5876e8c4f7fdb
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-13 23:28:08 -0300

    Fazendo

--- Estrututa_Decisao/Inteiro_menor.py
+++ Estrututa_Decisao/Inteiro_menor.py
@@ -1,7 +1,3 @@
 numero=int(input('Digite um número positivo menor que 1000: '))
-unidade=numero%10
-dezena=numero%10
-centena=numero
-numero=(numero-unidade)/10
-numero=(numero-dezena)/10
-print(f'{centena} centena(s),{dezena} dezena(s), {unidade} unidade(s)')
+if numero>=1000:
+    print('Número inválido.')
