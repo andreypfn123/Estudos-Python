0384998053a68efad41085d09b70657564172817
Andrey <andreypfn123@hotmail.com>
2022-09-21 12:57:08 -0300

    Pronto 9

--- Estrutura_Repeticao/Ordem_Cres.py
+++
@@ -1,6 +0,0 @@
-num1=float(input('Primeiro número: '))
-num2=float(input('Segundo número: '))
-num3=float(input('Terceiro número: '))
-numeros=[num1,num2,num3]
-numeros.sort()
-print(f'Os número em ordem crescente são: {numeros}')
