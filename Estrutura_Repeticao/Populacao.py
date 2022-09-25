8a6ba26e904ea1aec3f9c12a08c9815b036d656d
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-24 22:28:18 -0300

    Pronto 4

--- Estrutura_Repeticao/Populacao.py
+++ Estrutura_Repeticao/Populacao.py
@@ -1,3 +1,9 @@
-pais_a
-pais_b
-anos
+pais_a=80000
+pais_b=200000
+anos=0
+taxa_crescimento=1.5
+while pais_a<=pais_b:
+    pais_a+=pais_a*0.03
+    pais_b+=pais_b*0.015
+    anos+=1
+print(f'O Pais A ultrapassa o Pais B em {anos} anos')
